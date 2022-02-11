ee6a4e42eab14a91af8b06bd000a0146957bfa03
adeankush3 <adeankush3@gmail.com>
2022-02-11 20:43:42 +0530

    [Amkusk] Generic Demo program

---
+++ GenricDemo/GenricDemo/GenericDemo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenricDemo
+{
+    public class GenericDemo <T>
+    {
+        public void Print(T[] array)
+        {
+            foreach(T item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
---
+++ GenricDemo/GenricDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenricDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] doubleArray = { 1.2, 2.2, 3.3, 4.4, 5.5 };
+            char[] charArray = { 'a', 'b', 'c', 'd' };
+            GenericDemo<double> demo = new GenericDemo<double>();
+            GenericDemo<char> demo1 = new GenericDemo<char>();
+            demo.Print(doubleArray);
+            demo1.Print(charArray);
+            Program.toPrint(doubleArray);
+            Program.toPrint(charArray);
+        }
+        public static void toPrint(double[] inputArray)
+        {
+            foreach(double element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("--------");
+        }
+        public static void toPrint(char[] inputArray)
+        {
+            foreach (char element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("--------");
+        }
+
+    }
+}
